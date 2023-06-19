8bb478553444bf8d82d5372e7e967ec893bc5d5a
unknown <doniaskima344@gmail.com>
2023-06-19 23:36:50 +0100

    feat(Prisma Module)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,9 +2,11 @@
  
 import { Injectable } from '@nestjs/common';
 import { User ,Bookmark } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
+  constructor(private prisma:PrismaService){}
   signin() {
     return { msg: 'I have signed in' };
   }
