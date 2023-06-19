d796baad37101cde4fb5720e3a74e2228c3b00f7
unknown <doniaskima344@gmail.com>
2023-06-20 00:46:14 +0100

    feat(NestJs global pipe)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@
 import { Injectable } from '@nestjs/common';
 import { User ,Bookmark } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -10,7 +11,7 @@ export class AuthService {
   signin() {
     return { msg: 'I have signed in' };
   }
-  signup(){
+  signup(dto :AuthDto){
     
     return { msg: 'I have signed up' };
   }
