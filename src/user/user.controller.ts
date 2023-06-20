e3739b83ac1c0b2c5eda2aeaa4e3cf8934da4b7d
unknown <doniaskima344@gmail.com>
2023-06-20 16:41:39 +0100

    feat(NestJs custom param decorator)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,25 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get ,UseGuards } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    Patch,
+    UseGuards,
+  } from '@nestjs/common';
 import {AuthGuard} from '@nestjs/passport';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
-
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-    @UseGuards(JwtGuard)
-    @Get('me')
-    getMe() {
-       return 'return user info'
-    }
+  
+  @Get('me')
+  getMe(@GetUser() user: User) {
+    return user;
+  }
+
+  @Patch()
+  editUser() {}
+  
 }
