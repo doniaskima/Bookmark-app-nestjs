7d4bafb2991c92c80051df92b5fa1e312c08d549
unknown <doniaskima344@gmail.com>
2023-06-20 13:37:14 +0100

    feat(signup logic)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,10 @@
 /* eslint-disable prettier/prettier */
  
-import { Injectable } from '@nestjs/common';
-import { User ,Bookmark } from '@prisma/client';
+import { Injectable , ForbiddenException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
@@ -11,9 +12,33 @@ export class AuthService {
   signin() {
     return { msg: 'I have signed in' };
   }
-  signup(dto :AuthDto){
-    
-    return { msg: 'I have signed up' };
+ 
+  async signup(dto: AuthDto) {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+    // save the new user in the db
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (
+        error instanceof
+        PrismaClientKnownRequestError
+      ) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException(
+            'Credentials taken',
+          );
+        }
+      }
+      throw error;
+    }
   }
 
 }
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,15 @@
-/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
-      super();
-    datasources: {
+    super({
+      datasources: {
         db: {
-            url: 'postgresql://postgres:123@localhost:5434/nest?schema=public'
+          url: 'postgresql://postgres:123@localhost:5434/nest?schema=public'
         }
-    }
+      }
+    });
   }
 }
