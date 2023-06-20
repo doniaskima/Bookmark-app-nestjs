0048483ed1d87dff9d34e973385cc892fa71e750
unknown <doniaskima344@gmail.com>
2023-06-20 14:20:25 +0100

    feat(automate postgres restart and prisma migrations)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,26 @@
 /* eslint-disable prettier/prettier */
-import {Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
-@Controller('auth') 
+@Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {} 
+  constructor(private authService: AuthService) {}
+
   @Post('signup')
   signup(@Body() dto: AuthDto) {
     return this.authService.signup(dto);
   }
-  @Post('signin')
-  signin() {
-    return this.authService.signin();
-  }
 
+  @HttpCode(HttpStatus.OK)
+  @Post('signin')
+  signin(@Body() dto: AuthDto) {
+    return this.authService.signin(dto);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,10 +9,36 @@ import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 @Injectable()
 export class AuthService {
   constructor(private prisma:PrismaService){}
-  signin() {
-    return { msg: 'I have signed in' };
+  async signin(dto: AuthDto) {
+    // find the user by email
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          email: dto.email,
+        },
+      });
+    // if user does not exist throw exception
+    if (!user)
+      throw new ForbiddenException(
+        'Credentials incorrect',
+      );
+
+    // compare password
+    const pwMatches = await argon.verify(
+      user.hash,
+      dto.password,
+    );
+    // if password incorrect throw exception
+    if (!pwMatches)
+      throw new ForbiddenException(
+        'Credentials incorrect',
+      );
+
+      delete user.hash;
+      return user;
+   
   }
- 
+
   async signup(dto: AuthDto) {
     // generate the password hash
     const hash = await argon.hash(dto.password);
